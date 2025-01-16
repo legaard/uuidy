ee1962635aafa76892594c39f46a210e64363f8e
Lasse Legaard <legaard.lasse@gmail.com>
2025-01-16 20:50:19 +0100

    Added test of V4

--- cmd/cmd_uuid_test.go
+++ cmd/cmd_uuid_test.go
@@ -159,3 +159,59 @@ func TestV3Cmd(t *testing.T) {
 		assert.UUIDVersion(t, string(actual), 3)
 	})
 }
+
+func TestV4Cmd(t *testing.T) {
+	t.Run(`use is "v4"`, func(t *testing.T) {
+		// arrange
+		var (
+			sut = cmd.V4Cmd()
+		)
+
+		// act
+		actual := sut.Use
+
+		// assert
+		assert.Equal(t, actual, "v4")
+	})
+
+	t.Run("generate UUID", func(t *testing.T) {
+		// arrange
+		var (
+			writerMock = &WriterMock{}
+			sut        = cmd.V4Cmd()
+		)
+		sut.SetOut(writerMock)
+
+		// act
+		err := sut.RunE(sut, []string{"testing"})
+
+		// assert
+		assert.NoError(t, err)
+		assert.Equal(t, len(writerMock.WriteCalls()), 1)
+
+		actual := writerMock.WriteCalls()[0].P
+		assert.UUIDVersion(t, string(actual), 4)
+	})
+
+	t.Run("generate multiple UUIDs", func(t *testing.T) {
+		// arrange
+		var (
+			writerMock = &WriterMock{}
+			number     = 10
+			sut        = cmd.V4Cmd()
+		)
+		sut.SetOut(writerMock)
+		_ = sut.Flags().Set(cmd.FlagNumber, fmt.Sprintf("%d", number))
+
+		// act
+		err := sut.RunE(sut, []string{"testing"})
+
+		// assert
+		assert.NoError(t, err)
+		assert.Equal(t, len(writerMock.WriteCalls()), number)
+		for _, call := range writerMock.WriteCalls() {
+			actual := strings.ReplaceAll(string(call.P), "\n", "") // remove new lines
+			assert.UUIDVersion(t, actual, 4)
+		}
+	})
+}
